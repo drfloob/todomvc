e2cbd967dfea74797c9c87e0d1252df293730225
aj heller <aj@drfloob.com>
2014-01-30 17:41:14 -0800

    "clear completed" footer button updates and works.

--- labs/dependency-examples/react_observejs_require/js/views/footer.js
+++ labs/dependency-examples/react_observejs_require/js/views/footer.js
@@ -4,14 +4,15 @@ define(['react'], function(React) {
 
     var UIFooter = React.createClass({
 
-        componentWillMount: function() {
-        },
-
         render: function() {
-            var total = this.props.todos.length;
-            var completed = this.props.todos.length;
+            console.log('rendering footer');
 
-            var fStyle ={visibility: total > 0 ? 'visible' : 'hidden'}
+            var completed = this.props.todos.filter(function(t){return t.completed;});
+
+            var totalCount = this.props.todos.length;
+            var completedCount = completed.length;
+
+            var fStyle ={visibility: totalCount > 0 ? 'visible' : 'hidden'}
             return ( 
 		    <footer style={fStyle} id="footer">
 		    {/*<!-- This should be `0 items left` by default -->*/}
@@ -30,11 +31,44 @@ define(['react'], function(React) {
                     </ul>
 
                     {/*<!-- Hidden if no completed items are left ↓ -->*/}
-                    <button id="clear-completed">Clear completed ({completed})</button>
+                    <button id="clear-completed" onClick={this.handleClearCompleted} >Clear completed ({completedCount})</button>
 
                     </footer>
             );
+        },
+
+        componentWillMount: function() {
+            this.watchTodos(this, this.props);
+        },
+
+        componentWillUpdate: function(nextProps) {
+            this.unwatchTodos(this, this.props);
+            this.watchTodos(this, nextProps);
+        },
+
+        componentWillUnmount: function() {
+            this.unwatchTodos(this, this.props);
+        },
+
+        watchTodos: function(self, props) {
+            props.cb = function() {
+                self.setState();
+            };
+            props.todos.forEach(function(t) {
+                Object.observe(t, props.cb);
+            });
+        },
+
+        unwatchTodos: function(self, props) {
+            props.todos.forEach(function(t) {
+                Object.unobserve(t, props.cb);
+            });
+        },
+
+        handleClearCompleted: function() {
+            this.props.todos.filter(function(t) { return t.completed }).forEach(function(t) { t.delete(); });
         }
+        
 
     });
 
